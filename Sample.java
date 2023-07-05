87296f8512411cb660411360ce867691f4d968a6
Priyanshi9084 <118610570+Priyanshi9084@users.noreply.github.com>
2023-07-05 23:03:39 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Library;
+public class Sample {
+    public static void main(String args[]){
+        System.out.println("Helo wrol");
+    }
+}
